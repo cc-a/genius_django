4b1c138ad3d8216385c96e8b2f586e6b094aacb4
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2022-09-04 18:13:38 +0100

    Finalise stats module

--- stats/__init__.py
+++ stats/__init__.py
@@ -1,5 +1,8 @@
+from collections import namedtuple
 from statistics import mean, median, mode
+
+Stats = namedtuple("stats", ("mean", "median", "mode"))
 
 
 def calc_stats(data):
-    return (mean(data), median(data), mode(data))
+    return Stats(mean(data), median(data), mode(data))
