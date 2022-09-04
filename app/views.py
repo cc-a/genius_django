1e1b7507749282bcfa6623fdbe99306090124c51
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2022-09-04 18:08:58 +0100

    Add docstrings for view functions

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from .models import AccessRecord
 
 
 def info(request):
+    """Display/process form with new user information."""
     form = UserInfoForm(request.POST or None)
     if request.method == "POST" and form.is_valid():
         form.save()
@@ -12,9 +13,11 @@ def info(request):
 
 
 def index(request):
+    """Basic index page."""
     return render(request, "home.html")
 
 
 def access(request):
+    """Page that logs visits."""
     AccessRecord.objects.create()
     return render(request, "access.html")
